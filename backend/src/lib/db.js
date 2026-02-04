7f3d72865b3dacef4c407a08bd99e09b43ed0c5f
Saurabh <shivamrajjaiker@gmail.com>
2026-02-04 11:24:13 +0530

    Update backend DB config and frontend axios setup

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -1,9 +1,14 @@
-import mongoose from "mongoose"
-export const connectDB=async ()=>{
-    try {
-       const conn= await mongoose.connect(process.env.MONGODB_URI);
-       console.log(`MongoDB connected : ${conn.connection.host}`)
-    } catch (error) {
-        console.log("MongoDb connection error",error)
-    }
-}
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+  try {
+    console.log("URI CHECK:", process.env.MONGO_URI);
+
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log("MongoDb connection error", error);
+    process.exit(1);
+  }
+};
